42fb57b348deacefd976c0cc46c827aee1581bef
Marc BILICK BASSI <bilickbassi@gmail.com>
2025-01-11 23:00:44 +0100

    updates

--- deploy-ec2-instance/ec2.tf
+++ deploy-ec2-instance/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-049a0782ca02dc58c"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.ec2_sg.name] 
